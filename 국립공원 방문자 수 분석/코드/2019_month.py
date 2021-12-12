8535a57ac2de8cc7bb83fa2724693c4a6f59a053
ms9648 <43836180+ms9648@users.noreply.github.com>
2021-12-12 16:51:26 +0900

    PROJECT_UPDATE

--- 국립공원 방문자 수 분석/코드/2019_month.py
+++ 국립공원 방문자 수 분석/코드/2019_month.py
@@ -1,13 +1,35 @@
+# 전처리 라이브러리
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
 
-name = '태백산'
-data = pd.read_csv("visitors_per_month_2019.csv", header = 0, index_col = 0)
-t_data = data.T[name][1:]
-plt.rc('font', family = 'Malgun Gothic')
-plt.rcParams['axes.unicode_minus'] = False
-plt.title('2019년 월별 ' + name + ' 국립공원 방문자 수 변화')
-plt.plot(t_data,'gray', label = name)
-plt.ylabel('방문자 수')
-plt.legend()
-plt.show()
+# 시각화 라이브러리
+import plotly.graph_objects as go
+# import plotly.offline as pyo
+# pyo.init_notebook_mode() -- 주피터 노트북에서 보여지도록 하는 부분
+import plotly.express as px
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('데이터 셋/visitors_per_month_2014.csv', encoding = 'utf-8-sig', index_col = 0)
+data = data.fillna(0)
+data = data.astype('int32')
+vertical = data.copy().T
+vertical = vertical.drop('합계') # 인덱스 '합계'행 제거
+Park_Name = '지리산'
+Target_Park = Park_Name + '국립공원'
+
+print(vertical.index)
+# print(vertical.index)
+# # plotly.graph_objs로 그래프 그리기
+# fig = go.Figure()
+# fig.add_trace(
+#     go.Scatter(
+#         x = vertical.index, y = vertical[Park_Name]
+#     )
+# )
+
+# fig.layout(
+#     name = Target_Park+'방문자 수'
+#     legend = True
+# )
+# fig.show()
